df8350f86ecbf6d453265702e80b10a917c8684b
xinyu <fangjingyu0502@163.com>
2019-08-08 14:21:42 +0800

    尝试

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,7 @@ const router = new Router({
         {
             path: '/home',
             name: 'Home',
+            meta:{auth:true,keepAlive: true},
             component: Home
         },
         {
@@ -41,13 +42,12 @@ const router = new Router({
 
 //  判断是否需要登录权限 以及是否登录
 router.beforeEach((to, from, next) => {
-    if (to.matched.some(res => res.meta.requireAuth)) {// 判断是否需要登录权限
+    if (to.matched.some(res => res.meta.auth)) {// 判断是否需要登录权限
         if (localStorage.getItem('username')) {// 判断是否登录
             next()
         } else {// 没登录则跳转到登录界面
             next({
-                path: '/Register',
-                query: { redirect: to.fullPath }
+                path: '/login',
             })
         }
     } else {
